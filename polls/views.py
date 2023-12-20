8197a013fb93ddab99ee18f595a94ea7fec70a95
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-12-20 14:42:06 +0300

    Lesson 22

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest
+from django.shortcuts import render, get_object_or_404
+
+from polls.models import Question
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello Django! This is polls app.")
+def index(request: HttpRequest):
+    questions = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': questions}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id: int):
+    question = get_object_or_404(Question, id=question_id)
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
+
+
 
