2a3553c5f4c7bd77e2c4ccd75397d20ced7060cb
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2023-12-20 15:25:18 +0300

    Homework 21: templates update

--- articles/views.py
+++ articles/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 # Create your views here.
 
-def articles(request):
+def index(request):
     context = {
         'articles': Article.objects.all()
     }
-    return render(request, 'articles.html', context)
+    return render(request, 'articles/index.html', context)
+
+def detail(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    return render(request, 'articles/detail.html', {'article': article})
