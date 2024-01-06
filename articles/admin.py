debeecbd85c14816270ebe3fe653a5adbec1921e
Maiseyeva Tatsiana <maiseyeva.tatsiana@gmail.com>
2024-01-06 20:51:22 +0300

    articles: fix admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,15 +1,23 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Author
+
+
 # Register your models here.
+
+# class AuthorInLine(admin.TabularInline):
+#     model = Author
+#     extra = 1
 
 class ArticleAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields': ['title', 'authors', 'like_count', 'is_popular']}),
+        (None, {'fields': ['title', 'like_count', 'is_popular']}),
         ('Date information', {'fields': ['text']})
     ]
     readonly_fields = ['like_count', 'is_popular']
-    search_fields = ['title', 'authors', 'text']
+    search_fields = ['title', 'text']
     list_display = ['title', 'is_popular']
+    # inlines = [AuthorInLine]
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Author)
